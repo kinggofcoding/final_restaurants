b6fd8859d8c0302bef75dcbec027b498c0cd814a
unknown <intuitionpz@gmail.com>
2023-12-21 16:17:53 +0800

    require dotenv in sequelize config & get index redirect

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const restaurant = require('./restaurant')
+const restaurant = require("./restaurant")
 
-router.use('/restaurants', restaurant)
+router.use("/restaurants", restaurant)
 
+router.get("/", (req, res) => {
+  res.redirect('/restaurants')
+})
 module.exports = router
