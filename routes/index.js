b5d729672288fe9a69c5cc854ec3de676ea70367
unknown <intuitionpz@gmail.com>
2023-12-21 17:03:02 +0800

    setup login/register routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,24 @@ const restaurant = require("./restaurant")
 router.use("/restaurants", restaurant)
 
 router.get("/", (req, res) => {
-  res.redirect('/restaurants')
+  res.redirect("/restaurants")
 })
+
+router.get("/register", (req, res) => {
+  res.render("register")
+})
+
+router.get("/login", (req, res) => {
+  res.render("login")
+})
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/restaurants",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+)
+
 module.exports = router
