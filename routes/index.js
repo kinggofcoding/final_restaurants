6a35f55d0c697a5cf4e1751446b278caee951a16
unknown <intuitionpz@gmail.com>
2023-12-22 15:07:00 +0800

    build login/register views

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const restaurant = require("./restaurant")
+const passport = require('passport')
 
 router.use("/restaurants", restaurant)
 
